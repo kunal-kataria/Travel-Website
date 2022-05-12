1de1b55de3729516c539b48edc340baf10f9d421
Kunal Kataria <kunalkataria1512@gmail.com>
2022-05-12 07:29:27 +0530

    Third

--- public/js/login.js
+++ public/js/login.js
@@ -22,3 +22,51 @@ regbtn.addEventListener('click', function handleClick() {
     log.style.display = 'block';
     reg.style.display = 'none';
 });
+
+
+// function validation(){
+
+//     const user = document.getElementById('username').value;
+//     const pass = document.getElementById('password').value;
+//     const name = document.getElementById('name').value;
+//     const mob = document.getElementById('mobileNo').value;
+    
+//     const uscheck = /^[A-Za-z.- ]{3,12}$/;
+//     const passcheck = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+//     const nmcheck = /^[A-Za-z]{3,12}$/;
+//     const mobcheck = /^[0-9]{10}$/;
+    
+//     if(uscheck.test(user)) {
+//         document.getElementById('usererror').innerHTML = "";
+//     }
+//     else {
+//         document.getElementById('usererror').innerHTML = "Username is nvalid.";
+//         return false;
+//     }
+    
+//     if(passcheck.match(pass)) {
+//         document.getElementById('passerror').innerHTML = "";
+//     }
+//     else {
+//         document.getElementById('passerror').innerHTML = "Password is nvalid.";
+//         return false;
+//     }
+    
+//     if(nmcheck.test(name)) {
+//         document.getElementById('nmerror').innerHTML = "";
+//     }
+//     else {
+//         document.getElementById('nmerror').innerHTML = "Name is nvalid.";
+//         return false;
+//     }
+    
+//     if(mobcheck.test(mob)) {
+//         document.getElementById('moberror').innerHTML = "";
+//     }
+//     else {
+//         document.getElementById('moberror').innerHTML = "Mobile no. is nvalid.";
+//         return false;
+//     }
+// }
+
+
